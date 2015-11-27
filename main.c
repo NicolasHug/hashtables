425777a7a2d0c3de12e76156041c8a5c5e46afa1
Nicolas Hug <nh.nicolas.hug@gmail.com>
2015-11-27 16:08:47 +0100

    added a test

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 
 #include "hashtables.h"
 
-#define SIZE 1024
-
-int main () {
+void test_1() {
   int i = 0;
   const int n_keys = 4;
+  const int size = 32;
   char * keys [] = {"key1",
                     "key2", 
                     "collision", 
                     "collisino",
                    };
 
-  ht_t * ht = init_ht(SIZE,
+  ht_t * ht = init_ht(size,
                       add_chaining,
                       lookup_chaining,
                       remove_chaining,
@@ -43,7 +42,37 @@ int main () {
   ht->remove(ht, "does_not_exist");
 
   ht->print(ht);
+}
 
+void test_2() {
+  const int size = 8;
+  ht_t * ht = init_ht(size,
+                      add_chaining,
+                      lookup_chaining,
+                      remove_chaining,
+                      print_chaining
+                      ); 
+  int ret = 0;
+  int i = 0;
+  char key [10];
+
+  /* collisions are inevitable here */
+  for (i = 0; i < size * 2; i++) {
+    sprintf(key, "%d", i);
+    ht->add(ht, key, i);
+  }
+
+  for (i = 0; i < size * 2; i++) {
+    sprintf(key, "%d", i);
+    ht->lookup(ht, key, &ret);
+    printf("%d -> %d\n", i, ret);
+  }
+}
+
+
+int main() {
+  test_1();
+  test_2();
 
   return 0;
 }
